f8eb93c2c083bba8be8b7d4bbf029545d490e83a
BrayanSGL <bsgaleanol@udistrital.edu.co>
2023-04-25 11:45:22 -0500

    mejora estetica del sidebar

--- gui.py
+++ gui.py
@@ -12,20 +12,31 @@ class GUI:
         # Crear la ventana principal
         self.root = Tk()
         self.root.title("Procesamiento de Imágenes Médicas")
-        
-        # Crear la barra lateral
-        self.image_listbox = Listbox(self.root)
-        self.image_listbox.pack(side=LEFT, fill=Y)
+
+        # Crear la barra lateral frame
+        self.sidebar = Frame(self.root, width=200, bg='white', height=600, padx=10, pady=10)
+        self.sidebar.pack(side=LEFT, fill=BOTH)
+
+        ## Label de la previsualización de la imagen
+        self.preview_label = Label(self.sidebar, text="Preview", bg='gray', width=12, height=12, anchor=CENTER)
+        self.preview_label.pack(side=TOP, fill=BOTH)
+
+        ## Lista de imágenes
+        self.image_listbox = Listbox(self.sidebar, width=30, height=20)
+        self.image_listbox.pack(side=TOP, fill=BOTH, expand=True)
         self.update_image_listbox()
         self.image_listbox.bind('<<ListboxSelect>>', self.image_selected)
-        
+
+        ## Botón para procesar imágen
+        self.load_button = Button(self.sidebar, text="Procesar imágen", command=self.process_image)
+        self.load_button.pack(side=BOTTOM, fill=X)
+        self.load_button.config(state=DISABLED)
+
+
         # Crear la zona de visualización de imágenes
         self.canvas = Canvas(self.root, width=600, height=600)
         self.canvas.pack(side=RIGHT, fill=BOTH, expand=True)
         
-        # Crear el botón para procesar imágenes
-        self.process_button = Button(self.root, text="Procesar Imagen", state=DISABLED, command=self.process_image)
-        self.process_button.pack(side=BOTTOM, padx=10, pady=10)
         
         # Mostrar la ventana principal
         self.root.mainloop()
@@ -43,15 +54,16 @@ class GUI:
         # Obtener la ruta de la imagen seleccionada
         image_path = self.image_loader.get_image_path(index)
         
-        # Cargar la imagen seleccionada en la zona de visualización
-        self.current_image = Image.open(image_path)
-        self.current_image = self.current_image.resize((600, 600))
-        self.current_image = ImageTk.PhotoImage(self.current_image)
-        self.canvas.delete("all")
-        self.canvas.create_image(0, 0, anchor=NW, image=self.current_image)
+        # Cargar la imagen seleccionada en la zona de visualización de la barra lateral
+
+        # self.current_image = Image.open(image_path)
+        # self.current_image = self.current_image.resize((600, 600))
+        # self.current_image = ImageTk.PhotoImage(self.current_image)
+        # self.canvas.delete("all")
+        # self.canvas.create_image(0, 0, anchor=NW, image=self.current_image)
         
         # Habilitar el botón de procesar imagen
-        self.process_button.config(state=NORMAL)
+        self.load_button.config(state=NORMAL)
         
     def process_image(self):
         pass
